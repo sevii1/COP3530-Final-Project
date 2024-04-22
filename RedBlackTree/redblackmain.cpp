a0f248506b060d288e642edd0da435627623d4b4
gmat98 <giovanni.matias@ufl.edu>
2024-04-22 18:18:17 -0400

    more refinement

--- RedBlackTree/redblackmain.cpp
+++ RedBlackTree/redblackmain.cpp
@@ -1,75 +1,33 @@
 #include <iostream>
 #include "redblacktree.h"
 
+using namespace std;
+
 int main() {
     RBT tree;
 
-    // Insert some nodes into the tree
+ 
     tree.insert("13", "13");
-    //13, 8, 7, 1, 123, 4, 9, 19
-    std::cout << "In-order traversal of the tree: ";
-    tree.printInOrder();
-    std::cout << std::endl;
-
     tree.insert("8", "8");
-
-    std::cout << "In-order traversal of the tree: ";
-    tree.printInOrder();
-    std::cout << std::endl;
-
     tree.insert("7", "7");
-
-    std::cout << "In-order traversal of the tree: ";
-    tree.printInOrder();
-    std::cout << std::endl;
-
     tree.insert("1", "1");
-
-    std::cout << "In-order traversal of the tree: ";
-    tree.printInOrder();
-    std::cout << std::endl;
-
     tree.insert("123", "123");
-
-    std::cout << "In-order traversal of the tree: ";
-    tree.printInOrder();
-    std::cout << std::endl;
-
     tree.insert("4", "4");
-
-    std::cout << "In-order traversal of the tree: ";
-    tree.printInOrder();
-    std::cout << std::endl;
-
     tree.insert("300", "300");
 
-    std::cout << "In-order traversal of the tree: ";
     tree.printInOrder();
-    std::cout << std::endl;
+   
+    int id = 123;
 
-
-
-
-    // Print the tree in order
-    std::cout << "In-order traversal of the tree: ";
-    tree.printInOrder();
-    std::cout << std::endl;
-
-
-
-    // Search for a node by ID
-    int idToSearch = 789;
-    RBT::TreeNode* foundNode = tree.searchID(tree.getRoot(), idToSearch);
-    if (foundNode != nullptr) {
-        std::cout << "Found node with ID " << idToSearch << ": " << foundNode->NAME << std::endl;
-    }
-    else {
-        std::cout << "Node with ID " << idToSearch << " not found." << std::endl;
+    TreeNode* node = tree.searchID(tree.getRoot(), id);
+    if (node != nullptr) {
+        cout << "Node with ID " << id << ": " << node->NAME << endl;
+    }else {
+        cout << "Node with ID " << id << " not found." << endl;
     }
 
-    // Search for a node by name
-    std::string nameToSearch = "Alice";
-    std::cout << "Searching for node with name \"" << nameToSearch << "\": ";
+    string name = "Alice";
+    cout << "Node with name- "<< name << ": ";
     tree.searchNAME(tree.getRoot(), nameToSearch);
 
     return 0;
